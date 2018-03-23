b92b6f9729810f7604c7fe5c94dd0e2e3a938f3e
Raphael Ribeiro Tutui <31199384+raphatt@users.noreply.github.com>
2018-03-23 17:16:40 -0300

    Versão 1

---
+++ Youtube Beta/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Youtube_Beta
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(177, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(495, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.textBox1_MouseClick);
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(678, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Pesquisar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(150, 364);
+            this.flowLayoutPanel1.TabIndex = 4;
+            this.flowLayoutPanel1.WrapContents = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(180, 41);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(576, 335);
+            this.panel1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(768, 394);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Youtube";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Youtube Beta/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Reflection;
+using System.Threading;
+using CefSharp;
+using CefSharp.WinForms;
+
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Upload;
+using Google.Apis.Util.Store;
+using Google.Apis.YouTube.v3;
+using Google.Apis.YouTube.v3.Data;
+
+namespace Youtube_Beta
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();            
+        }
+
+        ChromiumWebBrowser chrome;
+
+        public async void button2_Click(object sender, EventArgs e)
+        {
+            this.Cursor = Cursors.AppStarting;
+            flowLayoutPanel1.Controls.Clear();
+            var youtubeService = new YouTubeService(new BaseClientService.Initializer()
+            {
+                ApiKey = "AIzaSyB82SdueHZ - KNAEnjkfhcZDCMwkm_ahg2o",
+                ApplicationName = this.GetType().ToString()
+            });
+            var searchListRequest = youtubeService.Search.List("snippet");
+            searchListRequest.Q = textBox1.Text;
+            searchListRequest.Type = "video";
+            searchListRequest.MaxResults = 15;
+            var searchListResponse = await searchListRequest.ExecuteAsync();
+            foreach (var searchResult in searchListResponse.Items)
+            {
+                PictureBox thumb = new PictureBox();
+                thumb.Size = new Size(120, 90);
+                thumb.ImageLocation = searchResult.Snippet.Thumbnails.Default__.Url;
+                thumb.Cursor = Cursors.Hand;
+                TextBox videoNome = new TextBox();
+                videoNome.Size = new Size(120, 45);
+                videoNome.BorderStyle = BorderStyle.None;
+                videoNome.BackColor = SystemColors.Control;
+                videoNome.TextAlign = HorizontalAlignment.Center;
+                videoNome.Multiline = true;
+                videoNome.Cursor = Cursors.Hand;
+                videoNome.Text = searchResult.Snippet.Title;
+                flowLayoutPanel1.Controls.Add(thumb);
+                flowLayoutPanel1.Controls.Add(videoNome);
+                thumb.Click += delegate
+                {
+                    string page = $"<html><body bgcolor='#f2f2f2'><iframe width='560' height='315' src='https://www.youtube.com/embed/{searchResult.Id.VideoId}?autoplay=1' frameborder='0' allow='autoplay; encrypted-media' allowfullscreen></iframe></body></html>";                    
+                    chrome.LoadHtml(page);
+                };
+                }
+            this.Cursor = Cursors.Default;
+            }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CefSettings settings = new CefSettings();
+            Cef.Initialize(settings);
+            chrome = new ChromiumWebBrowser("");
+            this.panel1.Controls.Add(chrome);
+            chrome.Dock = DockStyle.Fill;
+            chrome.LoadHtml("<html><body bgcolor='#f2f2f2'></body></html>");       
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Cef.Shutdown();
+        }
+
+        private void textBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            textBox1.Clear();
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Return)
+            {
+                button2_Click(sender, e);
+            }
+        }
+    }
+}
